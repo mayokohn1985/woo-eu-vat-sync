382c8b6a5878310b6f4ddf8568ff4bb6bc50651f
Marián Kohn <marian@kohn.sk>
2026-04-26 17:00:53 +0200

    Add insert logic and debug logging

--- includes/updater.php
+++ includes/updater.php
@@ -1,5 +1,11 @@
 <?php
 
+function wevs_log($message) {
+    if (defined('WP_DEBUG') && WP_DEBUG) {
+        error_log('[WEVS] ' . $message);
+    }
+}
+
 function wevs_update_all_vat_rates() {
 
     $rates = wevs_get_vat_rates();
@@ -11,20 +17,45 @@ function wevs_update_all_vat_rates() {
 
     foreach ($rates as $country => $rate) {
 
-        $result = $wpdb->update(
-            $table,
-            ['tax_rate' => $rate],
-            [
-                'tax_rate_country' => $country,
-                'tax_rate_name' => 'VAT'
-            ],
-            ['%f'],
-            ['%s', '%s']
-        );
+$existing = $wpdb->get_var($wpdb->prepare(
+    "SELECT tax_rate_id FROM $table 
+     WHERE tax_rate_country = %s 
+     AND tax_rate_name = %s",
+    $country,
+    'VAT'
+));
 
-        if ($result !== false) {
-            $updated++;
-        }
+if ($existing) {
+
+    $wpdb->update(
+        $table,
+        ['tax_rate' => $rate],
+        ['tax_rate_id' => $existing],
+        ['%f'],
+        ['%d']
+    );
+
+    wevs_log("UPDATED $country → $rate");
+
+} else {
+
+    $wpdb->insert(
+        $table,
+        [
+            'tax_rate_country'  => $country,
+            'tax_rate'          => $rate,
+            'tax_rate_name'     => 'VAT',
+            'tax_rate_priority' => 1,
+            'tax_rate_compound' => 0,
+            'tax_rate_shipping' => 1,
+            'tax_rate_order'    => 0,
+            'tax_rate_class'    => '',
+        ]
+    );
+
+    wevs_log("INSERTED $country → $rate");
+}
+
     }
 
     update_option('wevs_last_run', current_time('mysql'));
